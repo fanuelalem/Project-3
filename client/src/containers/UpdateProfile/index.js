fc4d0fe17ad30c1d351928565529888b5aa149a1
Fanuel Alem <fanuelnalem@outlook.com>
2021-01-30 22:10:31 -0800

    edit

--- client/src/containers/UpdateProfile/index.js
+++ client/src/containers/UpdateProfile/index.js
@@ -25,7 +25,7 @@ componentDidMount = () => {
 
 
 getMyImage = () => {
-    axios.get('/api/user/myimages',{headers: { 'authorization': localStorage.getItem('token')}})
+    axios.get('/api/user/myimages',{headers: { 'authorization': localStorage.getItem('token'),'mode':'cors'}})
     .then((response)=>{
         this.setState({myImages:response.data.reverse()})
         console.log(response)
